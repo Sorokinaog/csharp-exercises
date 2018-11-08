46ee3f53a8293bf85aec335a5644522b5ca4ffd4
Olga Sorokina <sorokinaog@icloud.com>
2018-11-08 01:00:11 -0500

    Studio 1 + Studio 2

---
+++ Area/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a radius:");
+            int radius = int.Parse(Console.ReadLine());
+
+            while (radius <= 0)
+            {
+                Console.WriteLine("A radius has to be positive");
+                Console.WriteLine("Enter a radius:");
+                radius = int.Parse(Console.ReadLine());
+
+            }
+
+            double pi = 3.14159;
+            double area = pi * radius * radius;
+
+            Console.WriteLine("The area of a circle with radius " + radius + " " +
+                    "is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CharCounter/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CharCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            string MyStr = InputStr.Replace(" ", string.Empty).ToLower();
+            Dictionary<string, int> Chars = new Dictionary<string, int>();
+
+            do
+            {
+                string OneChar = MyStr[0].ToString();
+                int count = (MyStr.Length - MyStr.Replace(OneChar, string.Empty).Length);
+                MyStr = MyStr.Replace(OneChar, string.Empty);
+                Chars.Add(OneChar, count);
+
+            } while (MyStr.Length != 0);
+
+            foreach (KeyValuePair<string, int> Char in Chars)
+            {
+                Console.WriteLine(Char.Key + ": " + Char.Value);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+}
