1fdc19c005ba8243c73a6ad4df4822e4403912aa
Olga Sorokina <sorokinaog@icloud.com>
2018-11-09 15:16:59 -0500

    Changed the names of all variables into C# name style

--- CharCounter/Program.cs
+++ CharCounter/Program.cs
@@ -7,20 +7,20 @@ namespace CharCounter
     {
         static void Main(string[] args)
         {
-            string InputStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
-            string MyStr = InputStr.Replace(" ", string.Empty).ToLower();
-            Dictionary<string, int> Chars = new Dictionary<string, int>();
+            string inputStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            string myStr = inputStr.Replace(" ", string.Empty).ToLower();
+            Dictionary<string, int> chars = new Dictionary<string, int>();
 
             do
             {
-                string OneChar = MyStr[0].ToString();
-                int count = (MyStr.Length - MyStr.Replace(OneChar, string.Empty).Length);
-                MyStr = MyStr.Replace(OneChar, string.Empty);
-                Chars.Add(OneChar, count);
+                string oneChar = myStr[0].ToString();
+                int count = myStr.Length - myStr.Replace(oneChar, string.Empty).Length;
+                myStr = myStr.Replace(oneChar, string.Empty);
+                chars.Add(oneChar, count);
 
-            } while (MyStr.Length != 0);
+            } while (myStr.Length != 0);
 
-            foreach (KeyValuePair<string, int> Char in Chars)
+            foreach (KeyValuePair<string, int> Char in chars)
             {
                 Console.WriteLine(Char.Key + ": " + Char.Value);
             }
